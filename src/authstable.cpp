c8020328d2a84fd1386995937513e970549e9dba
Marco <marco.fresco99@gmail.com>
2021-08-03 18:14:03 +0200

    Smart contract modified
    Improved auth checks, introduced erasetable func and changed table structure

--- src/authstable.cpp
+++ src/authstable.cpp
@@ -1,7 +1,6 @@
 #include <eosio/eosio.hpp>
 #include <json.hpp>
 
-
 using json = nlohmann::json;
 using namespace eosio;
 using namespace std;
@@ -20,18 +19,19 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
     /* The above declaration will extract the arguments of the action and 
     create necessary ABI struct descriptions in the generated ABI file. */
     [[eosio::action]]
-    void upsert( string owner, string perm, string account, string accperm, string attr) {
+    void upsert( string owner, string perm, string account, string accperm, 
+        uint64_t weight, string attr) {
         /* Only the owner has control over their own record (authority table), as this contract is opt-in. 
-        This method accepts an name type argument and asserts that the account executing the 
-        transaction equals the provided value and has the proper permissions to do so. */
-        require_auth( name(owner) );
-        // require_auth2 (owner, perm)
+        This method accepts two name type argument (account and permission) and asserts that the account 
+        executing the transaction equals the provided value and has the proper permissions to do so. */
+        require_auth( permission_level(name(owner), name(perm)) );
         /* To instantiate a table, two parameters are required:
         1. The first parameter specifies the owner of this table.
         2. The second parameter ensures the uniqueness of the table in the scope of this contract.*/
-        address_index records("authadmin"_n, get_first_receiver().value);
-        hash<string> hasher; string s = owner+perm+account;
-        uint64_t key = (uint64_t) hasher(s);
+        auths_index records("authadmin"_n, get_first_receiver().value);
+        hash<string> hasher; 
+        string s2 = owner+perm; string s1 = s2+account;
+        uint64_t key = (uint64_t) hasher(s1); uint64_t key_2 = (uint64_t) hasher(s2);
         auto iterator = records.find(key);
         if( iterator == records.end() ) {   // The user isn't in the table
             /* Create a record in the table using the multi_index method emplace. 
@@ -41,10 +41,12 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
             Inside the body assign the row's values with the ones provided to upsert. */
             records.emplace( name(owner), [&]( auto& row ) {
                 row.key = key;
+                row.key_2 = key_2;
                 row.owner = owner;
                 row.perm = perm;
                 row.account = account;
                 row.accperm = accperm;
+                row.weight = weight;
                 row.attr = attr;
             });
         }
@@ -56,10 +58,12 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
             3. The callback function that actually modifies the row. */
             records.modify( iterator, name(owner), [&]( auto& row ) {
                 row.key = key;
+                row.key_2 = key_2;
                 row.owner = owner;
                 row.perm = perm;
                 row.account = account;
                 row.accperm = accperm;
+                row.weight = weight;
                 row.attr = attr;
             });
         }
@@ -70,9 +74,9 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
     [[eosio::action]]
     void erase( string owner, string perm, string account, string accperm ) {
         /* A require_auth that tests against the action's argument user to verify only the 
-        owner of a record can modify their account. */
-        require_auth( name(owner) );
-        address_index records("authadmin"_n, get_first_receiver().value);
+        owner of a record with such permission can modify their account. */
+        require_auth( permission_level(name(owner), name(perm)) );
+        auths_index records("authadmin"_n, get_first_receiver().value);
         hash<string> hasher; string s = owner+perm+account;
         uint64_t key = (uint64_t) hasher(s);
         auto iterator = records.find(key);
@@ -84,11 +88,32 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
         records.erase(iterator);
     }
 
+    /* The above declaration will extract the arguments of the action and 
+    create necessary ABI struct descriptions in the generated ABI file. */
+    [[eosio::action]]
+    void erasetable( string owner, string perm ) {
+        /* A require_auth that tests against the action's argument user to verify only the 
+        owner of a record with such permission can modify their account. */
+        require_auth( permission_level(name(owner), name(perm)) );
+        auths_index records("authadmin"_n, get_first_receiver().value);
+        hash<string> hasher; string s = owner+perm;
+        uint64_t key_2 = (uint64_t) hasher(s);
+        auto iterator = records.begin();
+        /* A while loop is needed in way to erase all the records related to an authority table */
+        while ( iterator != records.end() ) {
+            /* Finally, call the erase method, to erase the iterator. 
+            Once the row is erased, the storage space will be free up for the original payer. */
+            if ( iterator->key_2 == key_2) iterator = records.erase(iterator);
+            else iterator++;
+        }
+    }
+
+
     [[eosio::action]]
     void viewattr( string owner, string perm, string account, string accperm ) {
     
-        require_auth ( name(owner) );
-        address_index records("authadmin"_n, get_first_receiver().value);
+        require_auth( permission_level(name(owner), name(perm)) );
+        auths_index records("authadmin"_n, get_first_receiver().value);
         hash<string> hasher; string s = owner+perm+account;
         uint64_t key = (uint64_t) hasher(s);
         auto iterator = records.find(key);
@@ -123,22 +148,37 @@ class [[eosio::contract("authstable")]] authstable : public eosio::contract {
             La correttezza è garantita dalla proprietà di iniettività delle funzioni hash,
             Dove per input diversi corrispondono risultati diversi. */
             uint64_t key;
+            /* Since the secondary index needs to be numeric field, a uint64_t altkey variable is added. 
+            This will represent the identity of an authtable starting from an owner and a perm,
+            in hash fashion */
+            uint64_t key_2;
             string owner;
             string perm;
             string account;
             string accperm;
+            uint64_t weight;
             string attr;
 
             /* Every multi_index struct requires a primary key method. 
             This method is used according to the index specification of your multi_index instantiation.*/
             uint64_t primary_key() const { return key;}
+            /* Getter for the secondary key */
+            uint64_t get_secondary_1() const { return key_2;}
+
         /* A table's data structure cannot be modified while it has data in it. 
         If you need to make changes to a table's data structure in any way, 
         you first need to remove all its rows */
         };
-        using address_index = eosio::multi_index<"authstable"_n, authority>;
+        using auths_index = eosio::multi_index<"authstable"_n, authority, 
+            indexed_by<"byauthtable"_n, const_mem_fun<authority, uint64_t, &authority::get_secondary_1>>>;
         /* With the above multi_index configuration there is a table named authstable, that
         1. Uses the _n operator to define an eosio::name type and uses that to name the table. 
         2. Pass in the singular authority struct defined in the previous step.
-        3. Declare this table's type. This type will be used to instantiate this table later. */
+        3. Declare this table's type. This type will be used to instantiate this table later. 
+        ––---------
+        In the third parameter, we pass a indexed_by struct which is used to instantiate a index.
+        In that indexed_by struct, we specify the name of index as "byauthtable" and the second type 
+        parameter as a function call operator which extracts a const value as an index key. 
+        In this case, we point it to the getter we created earlier so this multiple index table 
+        will index records by the altkey variable.*/
 };
